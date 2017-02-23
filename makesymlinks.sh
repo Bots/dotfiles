80a317cb78793ee82965e8a74d7639ab0d7f8e8c
John Paul Wile <botsone@gmail.com>
2017-02-23 21:22:43 +0000

    Added zshrc to script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc Xresources archey3.cfg"    # list of files/folders to symlink in homedir
+files="bashrc Xresources archey3.cfg zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
